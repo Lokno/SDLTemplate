1713ce7ede084d444818508f093578ac1132d4e3
legolasbat <legolasbat@gmail.com>
2025-01-29 22:09:03 +0000

    Init commit ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
-#include "SDL3/SDL_error.h"
-#include "SDL3/SDL_init.h"
-#include "SDL3/SDL_log.h"
 #include "SDL3/SDL_render.h"
-#include "SDL3/SDL_stdinc.h"
-#include "SDL3/SDL_video.h"
-#define SDL_MAIN_USE_CALLBACKS 1 // Use the callbacks instead of main()
+#include <imgui.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <backends/imgui_impl_sdlrenderer3.h>
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 
 typedef struct {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 } AppState;
 
-// This function runs once at startup.
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+
+int main(int argc, char* argv[]) {
 	// Init systems
 	if(!SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO)) {
 		SDL_Log("Couldn't initialize SDL: %s\n", SDL_GetError());
@@ -26,43 +22,72 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	if(!as) {
 		return SDL_APP_FAILURE;
 	}
-	*appstate = as;
 
 	if(!SDL_CreateWindowAndRenderer("Hello", 800, 600, 0, &as->window, &as->renderer)) {
 		SDL_Log("Couldn't craete window and renderer: %s\n", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
 
-	return SDL_APP_CONTINUE;
-}
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
-// This function runs once per frame, and is the heart of the program.
-SDL_AppResult SDL_AppIterate(void* appstate) {
-	AppState* as = (AppState*)appstate;
-	SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
-	SDL_RenderClear(as->renderer);
+	ImGui::StyleColorsDark();
 
-	SDL_RenderPresent(as->renderer);
+	ImGui_ImplSDL3_InitForSDLRenderer(as->window, as->renderer);
+	ImGui_ImplSDLRenderer3_Init(as->renderer);
 
-	return SDL_APP_CONTINUE;
-}
+	bool done = false;
 
-// This function runs when a new event (mouse input, keypresses, etc) occurs.
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
-	if(event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT) {
-		return SDL_APP_SUCCESS;
+	while(!done) {
+		// Event handle
+		SDL_Event event;
+		while(SDL_PollEvent(&event)) {
+			ImGui_ImplSDL3_ProcessEvent(&event);
+			
+			if(event.type == SDL_EVENT_KEY_DOWN || event.type == SDL_EVENT_QUIT) {
+				done = true;
+			}
+		}
+		
+		// Start frame
+		ImGui_ImplSDLRenderer3_NewFrame();
+		ImGui_ImplSDL3_NewFrame();
+		ImGui::NewFrame();
+
+		// Menu bar
+		if(ImGui::BeginMainMenuBar()) {
+			if(ImGui::BeginMenu("File")) {
+				if(ImGui::MenuItem("Close")) {
+					done = true;
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMainMenuBar();
+		}
+
+		// Render
+		ImGui::Render();
+		SDL_SetRenderDrawColor(as->renderer, 255, 0, 0, 255);
+		SDL_RenderClear(as->renderer);
+		ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), as->renderer);
+		SDL_RenderPresent(as->renderer);
+
 	}
-	return SDL_APP_CONTINUE;
-}
+	
+	// Cleanup
+	ImGui_ImplSDLRenderer3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
 
-// This function runs once at shutdown.
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-	if(appstate) {
-		AppState* as = (AppState*)appstate;
+	if(as) {
 		SDL_DestroyRenderer(as->renderer);
 		SDL_DestroyWindow(as->window);
 		SDL_free(as);
 	}
 
 	SDL_Quit();
+
+	return 0;
 }
